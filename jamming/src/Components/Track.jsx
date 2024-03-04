b0d75debb8dbe330f55896cedf85faabc5656a23
Gian Saccomanno <saccomannogian@gmail.com>
2024-03-04 17:44:58 -0500

    Plus/Minus Now Work
    Can now add to and remove from playlist, slight CSS styling

--- jamming/src/Components/Track.jsx
+++ jamming/src/Components/Track.jsx
@@ -1,46 +1,45 @@
-import React from "react";
+import React, { useCallback } from "react";
 import styles from '../CSS/Track.css';
 import Tracklist from "./Tracklist";
 import Playlist from "./Playlist";
 
 export default function Track(props) {
 
+    const passTrack = useCallback((event) => {
+        props.onAdd(props.track);
+    }, [props.onAdd, props.track]);
+
+
+    const passTrackToRemove = useCallback((event) => {
+        props.onRemove(props.track);
+    }, [props.onRemove, props.track]);
+
     function renderAction() {
-        if (props.removal) {
+        if (props.isRemoval) {
             return (
-                <button 
-                    onClick = {passTrackToRemove}
-                    className = "plus-minus"
-                >-</button>
+                <button onClick = {passTrackToRemove} className = "plus-minus">
+                    -
+                </button>
             );
         } else {
             return (
-                <button 
-                    onClick = {passTrack}
-                    className = "plus-minus"
-                >+</button>
+                <button onClick = {passTrack} className = "plus-minus">
+                    +
+                </button>
             );
         }
-    }
-
-    function passTrack() {
-        props.onAdd(props.track)
-    }
-
-    function passTrackToRemove() {
-        props.onRemove(props.track)
     }
 
     return (
         <div className="track">
             <div className="track-info">
                 <h3 className="song">{props.track.name}</h3>
-                <p className = "album">{props.track.artist} | {props.track.album}</p>
+                <div className = "info">
+                    <p className = "artist">{props.track.artist}</p>
+                    <p className = "album">{props.track.album}</p>
+                </div>
             </div>
-            <button
-                className = "plus-minus"
-            ></button>
             {renderAction()}
         </div>
     );
-}
+};
